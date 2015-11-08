f26557c0f46cdd88a18fe0e67051fcd8f2d7dfc3
kjparmar <kjparmar@uh.edu>
2015-11-08 16:55:56 -0600

    Added fully functional tweets-changed.py

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,1 +1,32 @@
-# example of program that calculates the number of tweets cleaned
+#!/usr/bin/python
+
+import sys
+
+def main():
+	inputFile = open('E:\git\coding-challenge-repo\\test\data-gen\\tweets.txt','r')
+	outputFile = open('E:\git\coding-challenge-repo\\test\\tweet_output\\ft1.txt', 'w')
+
+	counter = 0
+
+	for line in inputFile:
+
+		timeStamp = line.split('"', 4)
+		relevantText = line[line.find("\"text\":")+8:line.find("\",\"source")]			
+			
+		if "\u" in relevantText:
+			counter = counter + 1
+				
+		cleanText = relevantText.decode('unicode_escape').encode('ascii','ignore')
+		cleanText = cleanText.replace("\/", "/")
+		cleanText = cleanText.replace("\n", " ")
+		cleanText = " ".join(cleanText.split())
+
+		outputFile.write(cleanText + ' (timestamp: ' + timeStamp[3] + ')\n')
+			
+	outputFile.write(str(counter) + ' tweets contained unicode.')
+	
+	inputFile.close();
+	outputFile.close();
+
+if __name__ == '__main__':
+	main()
